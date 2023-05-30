44aad0ff65335bde98d6e353036304343e9d656d
ChrisTom-94 <christian.tomasino.dev@gmail.com>
2023-05-30 11:44:32 +0200

    prepare push release for folder for web hosting

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [glsl({root: './public'})],
-  base: '/power_4/',
+  // base: '/power_4/',
+  build: {
+    outDir: 'release'
+  }
 });
